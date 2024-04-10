99092e5cbe37e2309b0262560be5e3116addae7f
Kilian Stallinger <kilian.stallinger@findustrial.io>
2024-04-10 14:18:10 +0200

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import solidLogo from './assets/solid.svg'
 import { Show } from 'solid-js'
+import logo from './assets/image.png'
+import logoDark from './assets/image_dark.png'
 
 function App({ style, url }) {
 
@@ -9,25 +11,32 @@ function App({ style, url }) {
   const text = "Jetzt flexibel finanzieren statt kaufen!"
 
   // redirect to url
+  const buttonStyles = {
+    "default":  "inline-block rounded-[4px] pt-4 pb-3 px-5 text-base bg-[#003993] text-white text-center",
+    "dark":     "inline-block rounded-[4px] pt-4 pb-3 px-5 text-base bg-[#323232] text-white text-center",
+    "light":    "inline-block rounded-[4px] pt-4 pb-3 px-5 text-base border border-1 border-[#003992] bg-white text-center"
+  }
 
+  const textBaseStyle = "font-medium text-lg "
+  
   return (
     <>
       <Show when={_style === 'default'}>
-        <a href={_url} class="inline-block rounded-[4px] py-3 px-4 text-base bg-[#003993] text-white text-center">
-          <p class="font-medium leading-none">{text}</p>
-          <span class="font-normal text-xs tracking-wide leading-none">powered by Findustrial</span>
+        <a href={_url} class={buttonStyles[_style]}>
+          <p class={textBaseStyle}>{text}</p>
+          <span class="flex items-center justify-center font-normal text-xs tracking-wide">powered by <img src={logo} class="h-[21px] ml-1"></img></span>
         </a>
       </Show>
       <Show when={_style === 'dark'}>
-      <a href={_url} class="inline-block rounded-[4px] py-3 px-4 text-base bg-[#323232] text-white text-center">
-          <p class="font-medium leading-none">{text}</p>
-          <span class="font-normal text-xs tracking-wide leading-none">powered by Findustrial</span>
+      <a href={_url} class={buttonStyles[_style]}>
+          <p class={textBaseStyle}>{text}</p>
+          <span class="flex items-center justify-center font-normal text-xs tracking-wide">powered by <img src={logo} class="h-[21px] ml-1"></img></span>
         </a>
       </Show>
       <Show when={_style === 'light'}>
-      <a href={_url} class="inline-block rounded-[4px] py-3 px-4 text-base border border-1 border-[#003992] bg-white text-center">
-          <p class="font-medium leading-none text-[#003992]">{text}</p>
-          <span class="font-normal text-xs tracking-wide leading-none text-[#323232]">powered by Findustrial</span>
+      <a href={_url} class={buttonStyles[_style]}>
+          <p class={textBaseStyle + 'text-[#003992]'}>{text}</p>
+          <span class="flex items-center justify-center font-normal text-xs tracking-wide">powered by <img src={logoDark} class="h-[21px] ml-1"></img></span>
         </a>
         </Show>
     </>
